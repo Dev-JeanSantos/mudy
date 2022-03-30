89d1d64f986a297e8cc83c3c519b8896d2e83c82
Dev-JeanSantos <jeancbsan@gmail.com>
2022-03-30 13:16:59 -0300

    criação do projeto mudy

---
+++ src/main/java/br/com/treinamento/spring_security/mudy/MudyApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.spring_security.mudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudyApplication.class, args);
+	}
+
+}
