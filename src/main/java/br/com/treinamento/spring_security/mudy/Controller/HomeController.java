016c197962a8b31c4cfe63fe18ba1a3c394f2d1f
Dev-JeanSantos <jeancbsan@gmail.com>
2022-04-03 19:56:36 -0300

    refatoração JPA Repository

--- src/main/java/br/com/treinamento/spring_security/mudy/Controller/HomeController.java
+++ src/main/java/br/com/treinamento/spring_security/mudy/Controller/HomeController.java
@@ -1,26 +1,23 @@
 package br.com.treinamento.spring_security.mudy.Controller;
 
 import br.com.treinamento.spring_security.mudy.Modelo.Pedido;
+import br.com.treinamento.spring_security.mudy.Repository.HomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private HomeRepository repository;
 
     @GetMapping(value = "/home")
     public String hello(Model model) {
-        Query query = manager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-        List<Pedido> pedidos = query.getResultList();
+        List<Pedido> pedidos = repository.findAll();
         model.addAttribute("pedidos", pedidos);
         return "home";
     }
---
+++ src/main/java/br/com/treinamento/spring_security/mudy/Repository/HomeRepository.java
@@ -0,0 +1,10 @@
+package br.com.treinamento.spring_security.mudy.Repository;
+
+import br.com.treinamento.spring_security.mudy.Modelo.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HomeRepository extends JpaRepository<Pedido, Long> {
+
+}
