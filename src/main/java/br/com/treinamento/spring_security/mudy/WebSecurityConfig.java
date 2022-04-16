8d48dc59ae2f5f16e02bf8aa6f7444a14d18228e
Dev-JeanSantos <jeancbsan@gmail.com>
2022-04-16 17:34:31 -0300

    Implementado autenticação utilizando jdbc e configurei o database mysql realizando alguns inserts, criei as classes de usuarios e roles para cada usuario

--- src/main/java/br/com/treinamento/spring_security/mudy/WebSecurityConfig.java
+++ src/main/java/br/com/treinamento/spring_security/mudy/WebSecurityConfig.java
@@ -1,22 +1,24 @@
 package br.com.treinamento.spring_security.mudy;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Autowired
-//	private DataSource dataSource;
-//
+
+	@Autowired
+	private DataSource dataSource;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -28,42 +30,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 			.formLogin(form -> form
 				.loginPage("/login")
-				//.defaultSuccessUrl("/usuario/pedido", true)
+				.defaultSuccessUrl("/home", true)
 				.permitAll()
-        );
-//		.logout(logout -> {
-//			logout.logoutUrl("/logout")
-//				.logoutSuccessUrl("/home");
-//		}).csrf().disable();
+        )
+		.logout(logout -> {
+			logout.logoutUrl("/logout")
+				.logoutSuccessUrl("/home");
+		}).csrf().disable();
 	}
 
-	@Bean
 	@Override
-	protected UserDetailsService userDetailsService() {
-		UserDetails user =
-				User.withDefaultPasswordEncoder()
-						.username("jean")
-						.password("jean")
-						.roles("ADM")
-						.build();
-		return new InMemoryUserDetailsManager(user);
-	}
-//
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//		auth
-//			.jdbcAuthentication()
-//			.dataSource(dataSource)
-//			.passwordEncoder(encoder);
-//
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 //		UserDetails user =
 //				 User.builder()
-//					.username("jean")
-//					.password("jean")
-//					.password(encoder.encode("maria"))
+//					.username("mel")
+//					.password(encoder.encode("mel"))
 //					.roles("ADM")
 //					.build();
-//	}
+		auth
+				.jdbcAuthentication()
+				.dataSource(dataSource)
+				.passwordEncoder(encoder);
+//				.withUser(user);
+	}
 	
 }
